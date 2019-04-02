46fbf282b5ae51103bddfb68705d5eaed8f0347a
allenxz <13226271061@163.com>
2019-04-02 19:34:55 +0800

    制作goods组件，完成除购物车以外的大部分功能

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left"></div>
+      <div class="content-right"></div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" res="stylesheet/stylus">
+  
+</style>
